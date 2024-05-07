c1cfd0be998da6abc771ceeae43960cb709b55ce
PiCoTTo <danielgme@hotmail.com>
2024-05-07 05:07:25 +0200

    [Tools] - First approach to integrate nimo_scene_multiplier in CMake

---
+++ tools/nimo_scene_multiplier.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+#include "json.hpp"
+#include "core/Log.h"
+#include "glm/glm.hpp"
+#include "core/GUID.h"
+
+int main()
+{
+    nimo::Log::Initialize();
+    std::filesystem::path filepath = "Assets/Scenes/NewScene.nscene";
+    std::filesystem::path bakFilepath = "Assets/Scenes/NewScene.bak";
+
+    std::cout << "Reading scene file " << filepath;
+    std::ifstream file(filepath.c_str());
+
+    if (!file.good())
+    {
+        NIMO_INFO("Error opening file!: {}", strerror(errno));
+        return -1;
+    }
+
+    nlohmann::ordered_json scene;
+    scene << file;
+
+    nlohmann::ordered_json sceneCopy(scene);
+
+    NIMO_INFO("Multiplying the mesh entities...");
+    for (auto& entity : scene["Entities"].items())
+    {
+        NIMO_INFO("{}\n", entity.key());
+
+        if (entity.value().contains("Mesh"))
+        {
+            auto Translation = glm::vec3((float)entity.value()["Transform"]["Translation"][0], (float)entity.value()["Transform"]["Translation"][1], (float)entity.value()["Transform"]["Translation"][2]);
+
+            // Multiplies meshes
+            glm::vec3 offset(2, 0, 1);
+            int numOfCopies = 10;
+
+            nlohmann::ordered_json entityCopy(entity.value());
+
+            for (int i = -4; i < 5; ++i)
+            {
+                for (int j = -20; j < 0; ++j)
+                {
+                    nlohmann::ordered_json translation;
+                    translation["Translation"] = { Translation.x + i * offset.x, Translation.y, Translation.z + j * offset.z };
+                    nlohmann::ordered_json guid;
+                    guid["GUID"] = nimo::GUID::Create().Str();
+
+                    entityCopy.update(guid);
+                    entityCopy["Transform"].update(translation);
+
+                    sceneCopy["Entities"].push_back(entityCopy);
+                }
+            }
+        }
+    }
+
+    file.close();
+
+    if (std::rename(filepath.string().c_str(), bakFilepath.string().c_str()))
+    {
+        NIMO_INFO("Error renaming file: {} !!", strerror(errno));
+        return -1;
+    }
+
+    std::ofstream outFile(filepath);
+    outFile << sceneCopy.dump(1, '\t');
+    file.close();
+
+    NIMO_INFO("DONE!");
+}
