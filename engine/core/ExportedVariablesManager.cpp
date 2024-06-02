3e03850da4194c106da42c60de35a12b1ef490d6
PiCoTTo <danielgme@hotmail.com>
2024-06-02 23:41:40 +0200

    (Missing in previous commit) Added glm::vec3 type to the possible exportable variable types of the debugging system

--- engine/core/ExportedVariablesManager.cpp
+++ engine/core/ExportedVariablesManager.cpp
@@ -1,40 +1,52 @@
 #include "ExportedVariablesManager.h"
 
-std::shared_ptr<ExportedVariablesManager>& ExportedVariablesManager::instance()
+
+namespace nimo
 {
-	if (m_instance.get() == nullptr)
-		m_instance = std::make_shared<ExportedVariablesManager>();
+	std::shared_ptr<ExportedVariablesManager>& ExportedVariablesManager::instance()
+	{
+		if (m_instance.get() == nullptr)
+			m_instance = std::make_shared<ExportedVariablesManager>();
 
-	return m_instance;
+		return m_instance;
+	}
+
+	std::map<std::string, std::shared_ptr<ExportedVariable>> ExportedVariablesManager::variables()
+	{
+		return m_variables;
+	}
+
+	bool ExportedVariablesManager::addVariable(std::string name, std::shared_ptr<ExportedVariable>& variable)
+	{
+		if (m_variables.find(name) == m_variables.end())
+			m_variables[name] = variable;
+
+		return false;
+	}
+
+	bool ExportedVariablesManager::addVariable(std::string name, bool& variable)
+	{
+		if (m_variables.find(name) == m_variables.end())
+			m_variables[name] = std::make_shared<ExportedVariable>(name, typeid(bool), &variable);
+
+		return false;
+	}
+
+	bool ExportedVariablesManager::addVariable(std::string name, unsigned int& variable)
+	{
+		if (m_variables.find(name) == m_variables.end())
+			m_variables[name] = std::make_shared<ExportedVariable>(name, typeid(unsigned int), &variable);
+
+		return false;
+	}
+
+	bool ExportedVariablesManager::addVariable(std::string name, glm::vec3& variable)
+	{
+		if (m_variables.find(name) == m_variables.end())
+			m_variables[name] = std::make_shared<ExportedVariable>(name, typeid(glm::vec3), &variable);
+
+		return false;
+	}
+
+	std::shared_ptr<ExportedVariablesManager> ExportedVariablesManager::m_instance{ nullptr };
 }
-
-std::map<std::string, std::shared_ptr<ExportedVariable>> ExportedVariablesManager::variables()
-{
-	return m_variables;
-}
-
-bool ExportedVariablesManager::addVariable(std::string name, std::shared_ptr<ExportedVariable>& variable)
-{
-	if (m_variables.find(name) == m_variables.end())
-		m_variables[name] = variable;
-
-	return false;
-}
-
-bool ExportedVariablesManager::addVariable(std::string name, bool& variable)
-{
-	if (m_variables.find(name) == m_variables.end())
-		m_variables[name] = std::make_shared<ExportedVariable>(name, typeid(bool), &variable);
-
-	return false;
-}
-
-bool ExportedVariablesManager::addVariable(std::string name, unsigned int& variable)
-{
-	if (m_variables.find(name) == m_variables.end())
-		m_variables[name] = std::make_shared<ExportedVariable>(name, typeid(unsigned int), &variable);
-
-	return false;
-}
-
-std::shared_ptr<ExportedVariablesManager> ExportedVariablesManager::m_instance{ nullptr };
--- engine/core/ExportedVariablesManager.h
+++ engine/core/ExportedVariablesManager.h
@@ -3,28 +3,34 @@
 #include <map>
 #include <string>
 #include <typeindex>
+#include "glm/glm.hpp"
 
-struct ExportedVariable
+
+namespace nimo
 {
-	ExportedVariable(const std::string& name, const std::type_index& type, void* value) : m_name(name), m_type(type), m_value(value) {}
+	struct ExportedVariable
+	{
+		ExportedVariable(const std::string& name, const std::type_index& type, void* value) : m_name(name), m_type(type), m_value(value) {}
 
-	std::string m_name;
-	const std::type_index m_type;
-	void* m_value;
-};
+		std::string m_name;
+		const std::type_index m_type;
+		void* m_value;
+	};
 
-class ExportedVariablesManager
-{
-public:
-	static std::shared_ptr<ExportedVariablesManager>& instance();
+	class ExportedVariablesManager
+	{
+	public:
+		static std::shared_ptr<ExportedVariablesManager>& instance();
 
-	std::map<std::string, std::shared_ptr<ExportedVariable>> variables();
-	bool addVariable(std::string name, std::shared_ptr<ExportedVariable>& variable);
-	bool addVariable(std::string name, bool& variable);
-	bool addVariable(std::string name, unsigned int& variable);
+		std::map<std::string, std::shared_ptr<ExportedVariable>> variables();
+		bool addVariable(std::string name, std::shared_ptr<ExportedVariable>& variable);
+		bool addVariable(std::string name, bool& variable);
+		bool addVariable(std::string name, unsigned int& variable);
+		bool addVariable(std::string name, glm::vec3& variable);
 
-private:
-	static std::shared_ptr<ExportedVariablesManager> m_instance;
+	private:
+		static std::shared_ptr<ExportedVariablesManager> m_instance;
 
-	std::map<std::string, std::shared_ptr<ExportedVariable>> m_variables;
-};
+		std::map<std::string, std::shared_ptr<ExportedVariable>> m_variables;
+	};
+}
